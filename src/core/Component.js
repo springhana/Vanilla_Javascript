4ef80c72d01beccba5877001fc9af2d5e800d9fb
springhana <xksxksanfro7@naver.com>
2023-10-08 00:51:55 +0900

    first commit

---
+++ src/core/Component.js
@@ -0,0 +1,33 @@
+export default class Component {
+  $target;
+  props;
+  state;
+  constructor($target, props) {
+    this.$target = $target;
+    this.props = props; // props 할당
+    this.setup();
+    this.setEvent();
+    this.render();
+  }
+  setup() {}
+  mounted() {}
+  template() {
+    return "";
+  }
+  render() {
+    this.$target.innerHTML = this.template();
+    this.mounted(); // render 후에 mounted가 실행 된다.
+  }
+  setEvent() {}
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.render();
+  }
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+    this.$target.addEventListener(eventType, (event) => {
+      if (!event.target.closest(selector)) return false;
+      callback(event);
+    });
+  }
+}
